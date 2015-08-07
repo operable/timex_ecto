819c24974118615c0fd1c0f2f55e93019faef82a
vanstee <patrick@vanstee.me>
2015-08-07 12:59:46 -0400

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -29,10 +29,10 @@ defmodule TimexEcto.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:timex, "~> 0.16.0"},
-     {:ecto, "~> 0.14.2"},
+    [{:timex, "~> 0.17.0"},
+     {:ecto, "~> 0.15.0"},
      {:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.5", only: :dev}]
+     {:ex_doc, "~> 0.7.3", only: :dev}]
   end
 
   defp package do
